73c2cd284a790d36ea3796c29c4c1e6fdbc42375
Tim Freese <freese.tim@gmail.com>
2022-10-30 00:53:29 -0500

    Add project files.

---
+++ MTGAFriendMaker/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Windows;
+
+namespace MTGAFriendMaker
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void GetInfo_Click(object sender, RoutedEventArgs e)
+		{
+			var filePath = $"C:\\Users\\frees\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA\\Player.log";
+
+			string destinationFile = $"C:\\Users\\frees\\AppData\\LocalLow\\Wizards Of The Coast\\MTGA\\Player2.log";
+			try
+			{
+				File.Copy(filePath, destinationFile, true);
+			}
+			catch (IOException iox)
+			{
+				Console.WriteLine(iox.Message);
+			}
+
+			List<string> opponents = new List<string>();
+
+			foreach (var line in File.ReadAllLines(destinationFile))
+			{
+				if (line.Contains("playerName"))
+				{
+					var root = JsonSerializer.Deserialize<Root>(line);
+					var opponent = root.matchGameRoomStateChangedEvent.gameRoomInfo.gameRoomConfig.reservedPlayers.FirstOrDefault(x => x.playerName != "BigFreeze#94052").playerName;
+					opponents.Add(opponent);
+				}
+			}
+
+			PlayerName.Content = opponents.Last();
+			Clipboard.SetText(opponents.Last());
+		}
+	}
+}
---
+++ MTGAFriendMaker/Root.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTGAFriendMaker
+{
+	// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+	public class CheckpointConfiguration
+	{
+	}
+
+	public class ClipsConfiguration
+	{
+	}
+
+	public class ConnectionInfo
+	{
+		public string connectionState { get; set; }
+	}
+
+	public class GameRoomConfig
+	{
+		public string eventId { get; set; }
+		public List<ReservedPlayer> reservedPlayers { get; set; }
+		public string matchId { get; set; }
+		public GreConfig greConfig { get; set; }
+		public string greHostLoggerLevel { get; set; }
+		public int joinRoomTimeoutSecs { get; set; }
+		public int playerDisconnectTimeoutSecs { get; set; }
+	}
+
+	public class GameRoomInfo
+	{
+		public GameRoomConfig gameRoomConfig { get; set; }
+		public string stateType { get; set; }
+		public List<Player> players { get; set; }
+	}
+
+	public class GameStateRedactorConfiguration
+	{
+		public bool enableRedaction { get; set; }
+		public bool enableForceDiff { get; set; }
+	}
+
+	public class GreConfig
+	{
+		public GameStateRedactorConfiguration gameStateRedactorConfiguration { get; set; }
+		public ClipsConfiguration clipsConfiguration { get; set; }
+		public CheckpointConfiguration checkpointConfiguration { get; set; }
+	}
+
+	public class MatchGameRoomStateChangedEvent
+	{
+		public GameRoomInfo gameRoomInfo { get; set; }
+	}
+
+	public class Player
+	{
+		public string userId { get; set; }
+		public int systemSeatId { get; set; }
+	}
+
+	public class ReservedPlayer
+	{
+		public string userId { get; set; }
+		public string playerName { get; set; }
+		public int systemSeatId { get; set; }
+		public int teamId { get; set; }
+		public ConnectionInfo connectionInfo { get; set; }
+		public string courseId { get; set; }
+		public string sessionId { get; set; }
+		public string platformId { get; set; }
+	}
+
+	public class Root
+	{
+		public string transactionId { get; set; }
+		public int requestId { get; set; }
+		public string timestamp { get; set; }
+		public MatchGameRoomStateChangedEvent matchGameRoomStateChangedEvent { get; set; }
+	}
+
+
+}
